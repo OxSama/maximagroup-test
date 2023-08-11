6f50ef57b0d1654f6135be5869cde33e4583d430
OxSama <mhmdtageldin@gmail.com>
2023-08-11 20:49:59 +0200

    add tailwind css and vue to the porject

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -6,7 +6,7 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        <p class="text-red-500">I'm an example component.</p>
                     </div>
                 </div>
             </div>
@@ -15,9 +15,9 @@
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+export default {
+    mounted() {
+        console.log('Component mounted.')
     }
+}
 </script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,9 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <div id="app">
