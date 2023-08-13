15e9a11cf82c4e164244313554da316ba84015b2
OxSama <mhmdtageldin@gmail.com>
2023-08-13 17:24:18 +0400

    bug fix: move app to main

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 </head>
 
 <body>
-    <div id="app">
+    <div>
         <nav class="bg-white shadow-sm">
             <div class="container mx-auto px-4 py-2">
                 <div class="flex justify-between items-center">
@@ -29,7 +29,11 @@
 
                     <div class="md:hidden">
                         <button class="text-gray-500">
-                            <!-- Add some form of icon or visual for toggling menu in mobile. You can use a library like Heroicons. -->
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+                            </svg>
                         </button>
                     </div>
 
@@ -50,34 +54,70 @@
                                 __('Register') }}</a>
                             @endif
                             @else
-                            <div class="relative inline-block text-left">
-                                <div href="#"
-                                    class="inline-flex justify-center items-center space-x-2 text-gray-700 hover:text-gray-900">
+                            <div class="relative inline-block text-left mr-4">
+                                <button id="notificationButton" class="text-gray-500 focus:outline-none">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
+                                    </svg>
+                                </button>
+                                <div
+                                    class="dropdown origin-top-right absolute right-0 mt-2 w-64 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 hidden">
                                     <div class="py-1">
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                                            class="block px-4 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
-                                            {{ __('Logout') }}
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                            class="hidden">
-                                            @csrf
-                                        </form>
+                                        <a href="#"
+                                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Notification
+                                            1</a>
+                                        <a href="#"
+                                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Notification
+                                            2</a>
                                     </div>
                                 </div>
                             </div>
-                            @endguest
+                            <div
+                                class="inline-flex justify-center items-center space-x-2 text-gray-700 hover:text-gray-900">
+                                <div class="py-1">
+                                    <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                                        class="block px-4 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </div>
                         </div>
+                        @endguest
                     </div>
                 </div>
             </div>
         </nav>
+    </div>
 
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+    <main class="py-4" id="app">
+        @yield('content')
+    </main>
     </div>
 </body>
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        let notificationButton = document.getElementById('notificationButton');
+        let dropdown = document.querySelector('.dropdown');
+
+        // Toggle dropdown on button click
+        notificationButton.addEventListener('click', function () {
+            dropdown.classList.toggle('hidden');
+        });
+
+        // Close dropdown if clicked anywhere outside
+        document.addEventListener('click', function (event) {
+            if (!notificationButton.contains(event.target) && !dropdown.contains(event.target)) {
+                dropdown.classList.add('hidden');
+            }
+        });
+    });
+</script>
 
 </html>
