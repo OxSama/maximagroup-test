78dfb4e10f925f44c78a084c260441c64a74e7b2
OxSama <mhmdtageldin@gmail.com>
2023-08-14 05:30:26 +0400

    Authenticated and app Layouts for outer layouts of the components

---
+++ resources/js/layouts/AppLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <Header />
+    <section class="px-8">
+        <main class="container mx-auto">
+            <div class="lg:flex lg:justify-between">
+                <div class="lg:w-3/6"></div>
+                <div class="lg:flex-1 lg:mx-10" style="max-width: 700px">
+                    <slot></slot>
+                </div>
+            </div>
+        </main>
+    </section>
+</template>
+
+<script setup>
+import Header from '../components/Header.vue'
+// import MainContent from '../components/MainContent.vue'
+
+
+</script>
+
+<style scoped>
+/* Add any global styles here */
+</style>
---
+++ resources/js/layouts/AuthenticatedLayout.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <navbar :app-name="appName" :home-url="homeUrl">
+            <template v-slot:auth-links>
+                <notification-dropdown></notification-dropdown>
+                <logout-button :logout-route="logoutRoute"></logout-button>
+            </template>
+        </navbar>
+        <main class="py-4">
+            <slot></slot>
+        </main>
+    </div>
+</template>
+
+<script>
+// Component Imports
+import Navbar from '@/components/Navbar.vue';
+import NotificationDropdown from '@/components/Notifications.vue';
+import LogoutButton from '@/components/LogoutButton.vue';
+
+export default {
+    components: {
+        Navbar,
+        NotificationDropdown,
+        LogoutButton
+    },
+    data() {
+        return {
+            appName: "User Management",
+            homeUrl: "/",
+            logoutRoute: "/logout"
+        };
+    }
+};
+</script>
