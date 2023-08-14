f0444cbfacec5ea67c88414b0cdca6d4e1dc39eb
OxSama <mhmdtageldin@gmail.com>
2023-08-14 05:35:19 +0400

    dashboard page

---
+++ resources/js/views/DashboardView.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { onMounted, ref, watch } from "vue";
+import { useUsersStore } from "../stores/users";
+import TableComponent from "../components/TableComponent.vue";
+import AuthenticatedLayoutVue from "../layouts/AuthenticatedLayout.vue";
+
+
+        const usersStore = useUsersStore();
+
+        onMounted(async () => {
+            await usersStore.getUsers();
+        });
+
+        // As usersStore.users is reactive, you don't need a separate ref to watch it.
+        // However, if you wish to derive some computation from it, you can use computed or watch.
+        // Given the current scenario, it seems unnecessary.
+
+        function handleUpdate(userId) {
+            console.log('Update', userId);
+            // Implement your update logic here
+            // If you have update logic in the store, you can call it here.
+        }
+
+        async function handleDelete(userId) {
+            console.log('Delete', userId);
+            await usersStore.destroyUser(userId);
+            await usersStore.getUsers();
+        }
+
+
+
+</script>
+
+<template>
+    <AuthenticatedLayoutVue>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ 'Dashboard' }}</div>
+
+                        <div class="card-body">
+                            <TableComponent :users="usersStore.users" @update="handleUpdate" @delete="handleDelete" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayoutVue>>
+</template>
