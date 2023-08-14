ee3b356f578c38a326803c93353989ae67d3ab3d
OxSama <mhmdtageldin@gmail.com>
2023-08-14 05:27:46 +0400

    pinia main and user stores

---
+++ resources/js/stores/main.js
@@ -0,0 +1,97 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+import { useToast } from 'vue-toast-notification';
+
+
+axios.defaults.baseURL = '/api';
+
+export const useAuthStore = defineStore("auth", {
+    state: () => ({
+        user: {
+            id: null,
+            email: null,
+            name: null,
+            role: null,
+            role_id: null,
+        },
+        token: localStorage.getItem('token') || null,
+        roles: ["admin", "user"]
+    }),
+    actions: {
+        async login(credentials) {
+
+            try {
+                const response = await axios.post('/login', credentials, { excludeToken: true });
+                console.log(response);
+                if (response.data) {
+                    sessionStorage.setItem('token', response.data.token);
+                    useToast().open({ message: 'Logged In ! ', position: 'bottom-right', type: 'success' });
+                    this.token = response.data.token;
+                    this.user = response.data.user;
+                    return true;
+                }
+                else {
+                    if (response.status !== 200) {
+                        console.log(`Error: ${response.statusText}`);
+                        throw new Error(response.data.data);
+                    }
+                    if (response.data.code !== 200) {
+                        console.log(`Error: ${response.data.message}`);
+                        throw new Error(response.data.message);
+                    }
+                }
+            } catch (error) {
+                console.log(error);
+                useToast().open({ message:  error.response.data.error, position: 'bottom-right', type: 'error' });
+            }
+        },
+        logout() {
+            localStorage.removeItem('token');
+            this.token = null;
+            this.user = {
+                id: null,
+                email: null,
+                name: null,
+                role: null,
+                role_id: null,
+            };
+        },
+        async register(credentials) {
+            try {
+                const response = await axios.post('/register', credentials, { excludeToken: true });
+
+                if (response.status === 201 && response.data.token && response.data.user) {
+                    sessionStorage.setItem('token', response.data.token);
+                    useToast().open({ message: 'Logged In !', position: 'bottom-right', type: 'success' });
+                    this.token = response.data.token;
+                    this.user = response.data.user;
+                    return { success: true };
+                } else {
+                    console.log(`Error: ${response.data.message || 'Unexpected response structure'}`);
+                    throw new Error(response.data.message || 'Unexpected response structure');
+                }
+
+            } catch (error) {
+                return {
+                    success: false,
+                    data: error.response.data,
+                    message: error.message
+                };
+            }
+        },
+        async fetchUserData() {
+            if (this.token) {
+                try {
+                    const response = await axios.get('/user');
+                    this.user = response.data;
+                } catch (error) {
+                    if (error.response && error.response.data) {
+                        throw new Error(error.response.data.message || 'Error fetching user data');
+                    } else {
+                        throw new Error('Unable to fetch user data. Please try again.');
+                    }
+                }
+            }
+        }
+    }
+});
---
+++ resources/js/stores/users.js
@@ -0,0 +1,46 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+import { useToast } from 'vue-toast-notification';
+
+export const useUsersStore = defineStore({
+    // unique id of the store across your application
+    id: 'users',
+
+    state: () => ({
+        users: []
+    }),
+
+    actions: {
+        async getUsers() {
+            try {
+                const response = await axios.get("/users");
+                if (response.status === 200 && response.data.data) {
+                    this.users = response.data.data;
+                    return { success: true, data: this.users };
+                } else {
+                    console.log(`Error: ${response.data.message || 'Unexpected response structure'}`);
+                    throw new Error(response.data.message || 'Unexpected response structure');
+                }
+            } catch (error) {
+                useToast().open({ message: `Error fetching users: ${error.message}`, position: 'bottom-right', type: 'error' });
+            }
+        },
+
+        async destroyUser(id) {
+            try {
+                const response = await axios.delete("/users/" + id);
+                console.log(response)
+                if (response.status === 204) { // or whatever success status code the DELETE endpoint returns
+                    useToast().open({ message: 'User deleted successfully', position: 'bottom-right', type: 'success' });
+                    return { success: true };
+                } else {
+                    console.log(`Error: ${response.data.message || 'Unexpected response structure'}`);
+                    throw new Error(response.data.message || 'Unexpected response structure');
+                }
+            } catch (error) {
+                // useToast().open({ message: `Error deleting user: ${error.message}`, position: 'bottom-right', type: 'error' });
+            }
+        }
+    }
+
+});
