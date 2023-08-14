7be4878074ec258d8fd20309d9545450740d0499
OxSama <mhmdtageldin@gmail.com>
2023-08-14 05:51:08 +0400

    logout button for navigation

---
+++ resources/js/components/LogoutButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="inline-flex items-center justify-center space-x-2 text-gray-700 hover:text-gray-900">
+        <div class="py-1">
+            <a :href="logoutRoute" @click.prevent="logout"
+                class="block px-4 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+                Logout
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        logoutRoute: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        logout() {
+            // Implement your logout logic here
+        }
+    }
+};
+</script>
