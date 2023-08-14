1d88a57d465c08f0eab4c3a97524c9a5472f471b
OxSama <mhmdtageldin@gmail.com>
2023-08-14 05:33:07 +0400

    router routes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,13 +1,28 @@
+// Vue Router imports
 import { createWebHashHistory, createRouter } from "vue-router";
-import ExampleComponent from "../components/ExampleComponent.vue"
-import EditUserForm from "../components/EditUserForm.vue"
 
+// Component imports
+import EditUserForm from "../components/EditUserForm.vue";
+import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import UsersView from "../views/DashboardView.vue";
 
+// Define routes
 const routes = [
     {
         path: '/',
-        name: 'users.index',
-        component: ExampleComponent
+        name: 'login',
+        component: LoginView
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: RegisterView
+    },
+    {
+        path: '/users',
+        name: 'users.view',
+        component: UsersView
     },
     {
         path: '/edit-user',
@@ -16,7 +31,7 @@ const routes = [
     }
 ];
 
-
+// Create and export router instance
 export default createRouter({
     history: createWebHashHistory(),
     routes
