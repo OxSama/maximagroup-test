b339fbe152331de9392b3e0893cd3f2b69a947af
OxSama <mhmdtageldin@gmail.com>
2023-08-15 03:43:00 +0400

    edit users form

--- resources/js/components/EditUserForm.vue
+++ resources/js/components/EditUserForm.vue
@@ -1,31 +1,26 @@
 <template>
     <AuthenticatedLayoutVue>
         <div class="w-full max-w-xl mx-auto">
-            <form @submit.prevent="editUser">
+            <h2 class="text-2xl font-bold text-blue-600 my-5 border-b pb-3">
+                Edit User Named "{{ user.name }}" with id {{ user.id }}
+            </h2>
+            <form @submit.prevent="editUser(user.id)">
                 <div class="mb-4">
                     <label for="name" class="block mb-2 text-sm font-bold text-gray-700">Name:</label>
                     <input type="text" id="name" v-model="user.name" placeholder="Enter Name"
-                        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline">
+                        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline">
                 </div>
 
                 <div class="mb-4">
-                    <label for="email" class="block mb-2 text-sm font-bold text-gray-700">Email:</label>
-                    <input type="email" id="email" v-model="user.email" placeholder="Enter Email"
-                        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline">
+                    <label for="role" class="block mb-2 text-sm font-bold text-gray-700">Role:</label>
+                    <select id="role" v-model="user.role_id"
+                        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline">
+                        <option v-for="(role, index) in roles" :key="role.id" :value="index + 1">{{ role }}</option>
+                    </select>
                 </div>
 
-                <div class="mb-4">
-                    <label for="password" class="block mb-2 text-sm font-bold text-gray-700">Password:</label>
-                    <input type="password" id="password" v-model="user.password" placeholder="Enter Password"
-                        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline">
-                </div>
-
-                <div class="mb-6">
-                    <label for="confirmPassword" class="block mb-2 text-sm font-bold text-gray-700">Confirm
-                        Password:</label>
-                    <input type="password" id="confirmPassword" v-model="user.confirmPassword"
-                        placeholder="Confirm Password"
-                        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline">
+                <div v-if="errorMessage" class="text-red-500">
+                    {{ errorMessage }}
                 </div>
 
                 <div class="flex items-center justify-between">
@@ -37,43 +32,44 @@
 </template>
 
 
-<script>
+
+<script setup>
 import ButtonComponent from './BaseButton.vue';
 import AuthenticatedLayoutVue from '../layouts/AuthenticatedLayout.vue';
+import { useAuthStore } from '../stores/main';
+import { useUsersStore } from '../stores/users';
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import router from '../router';
 
-export default {
-    components: {
-        ButtonComponent,
-        AuthenticatedLayoutVue
-    },
-    props: {
-        userData: {
-            type: Object,
-            default: () => ({
-                name: '',
-                email: '',
-                password: '',
-                confirmPassword: ''
-            })
-        }
-    },
-    data() {
-        return {
-            user: { ...this.userData }
-        };
-    },
-    methods: {
-        editUser() {
-            if (this.user.password !== this.user.confirmPassword) {
-                alert('Passwords do not match!');
-                return;
-            }
 
-            // You can make an API call here to update the user data in the database.
-            // As before, in a real-world scenario, handle with proper validation, error handling, and feedback.
+const route = useRoute();
+const userId = ref(route.params.userId);
+const user = ref({});
+const store = useAuthStore();
+const errorMessage = ref('');
 
-            this.$emit('user-edited', this.user); // Emit an event with the updated user data
-        }
+const roles = ref(store.roles);
+
+onMounted(async () => {
+    try {
+        await store.fetchUserData(userId.value);
+        user.value = store.user;
+        console.log(user.value);
+    } catch (error) {
+        console.error("Error fetching user:", error.message);
+    }
+});
+
+async function editUser() {
+    try {
+        await store.updateUser({name:user.value.name, role_id:user.value.role_id},userId.value);
+        // Emit an event, show a success message, or navigate to another route
+        await useUsersStore().getUsers();
+        router.push("/users");
+    } catch (error) {
+        errorMessage.value = error.message; // Update the error message from the caught error
     }
 }
+
 </script>
