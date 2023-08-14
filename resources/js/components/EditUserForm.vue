a1dae6c277adff6444a5b6aad2854d2cb5170acb
OxSama <mhmdtageldin@gmail.com>
2023-08-14 06:16:39 +0400

    edit user form

--- resources/js/components/EditUserForm.vue
+++ resources/js/components/EditUserForm.vue
@@ -1,44 +1,50 @@
 <template>
-    <div class="w-full max-w-xl mx-auto">
-        <form @submit.prevent="editUser">
-            <div class="mb-4">
-                <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Name:</label>
-                <input type="text" id="name" v-model="user.name" placeholder="Enter Name"
-                       class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-            </div>
+    <AuthenticatedLayoutVue>
+        <div class="w-full max-w-xl mx-auto">
+            <form @submit.prevent="editUser">
+                <div class="mb-4">
+                    <label for="name" class="block mb-2 text-sm font-bold text-gray-700">Name:</label>
+                    <input type="text" id="name" v-model="user.name" placeholder="Enter Name"
+                        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline">
+                </div>
 
-            <div class="mb-4">
-                <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email:</label>
-                <input type="email" id="email" v-model="user.email" placeholder="Enter Email"
-                       class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-            </div>
+                <div class="mb-4">
+                    <label for="email" class="block mb-2 text-sm font-bold text-gray-700">Email:</label>
+                    <input type="email" id="email" v-model="user.email" placeholder="Enter Email"
+                        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline">
+                </div>
 
-            <div class="mb-4">
-                <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password:</label>
-                <input type="password" id="password" v-model="user.password" placeholder="Enter Password"
-                       class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-            </div>
+                <div class="mb-4">
+                    <label for="password" class="block mb-2 text-sm font-bold text-gray-700">Password:</label>
+                    <input type="password" id="password" v-model="user.password" placeholder="Enter Password"
+                        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline">
+                </div>
 
-            <div class="mb-6">
-                <label for="confirmPassword" class="block text-gray-700 text-sm font-bold mb-2">Confirm Password:</label>
-                <input type="password" id="confirmPassword" v-model="user.confirmPassword" placeholder="Confirm Password"
-                       class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-            </div>
+                <div class="mb-6">
+                    <label for="confirmPassword" class="block mb-2 text-sm font-bold text-gray-700">Confirm
+                        Password:</label>
+                    <input type="password" id="confirmPassword" v-model="user.confirmPassword"
+                        placeholder="Confirm Password"
+                        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline">
+                </div>
 
-            <div class="flex items-center justify-between">
-                <ButtonComponent label="Edit User" type="confirm" @click="editUser" />
-            </div>
-        </form>
-    </div>
+                <div class="flex items-center justify-between">
+                    <ButtonComponent label="Edit User" type="confirm" @click="editUser" />
+                </div>
+            </form>
+        </div>
+    </AuthenticatedLayoutVue>
 </template>
 
 
 <script>
 import ButtonComponent from './BaseButton.vue';
+import AuthenticatedLayoutVue from '../layouts/AuthenticatedLayout.vue';
 
 export default {
     components: {
-        ButtonComponent
+        ButtonComponent,
+        AuthenticatedLayoutVue
     },
     props: {
         userData: {
